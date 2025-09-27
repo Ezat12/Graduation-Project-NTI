cfc69c5a508c21bfced17288e9e0136657a45537
Amany-Elsayed <amanyelsayed0@outlook.com>
2025-09-27 14:12:43 +0300

    action done

--- server/src/config/connectionDB.js
+++ server/src/config/connectionDB.js
@@ -12,4 +12,5 @@ const connectionDB = () => {
     });
 };
 
+
 module.exports = connectionDB;
--- server/src/routes/course.route.js
+++ server/src/routes/course.route.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const {createCourse,getCourses,getCourseById} = require('../controllers/courseController');
+const {createCourse,getCourses,getCourseById, deleteLecture, deleteCourse, updateCourse} = require('../controllers/courseController');
 
 const router = express.Router();
 
 router.post('/', createCourse);
 router.get('/', getCourses);
 router.get('/:id', getCourseById);
+router.put("/:id", updateCourse);
+router.delete("/:id", deleteCourse);
+router.delete("/:courseId/lectures/:lectureId", deleteLecture);
 
 module.exports = router;
