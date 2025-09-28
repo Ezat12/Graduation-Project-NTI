dd1fd0ba35d838cf1481186fce4408bd28533de5
Ezat <ezatelbery164@gmail.com>
2025-09-28 23:48:00 +0300

    improve route structure

--- server/src/routes/course.route.js
+++ server/src/routes/course.route.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const {createCourse,getCourses,getCourseById} = require('../controllers/courseController');
+const express = require("express");
+const {
+  createCourse,
+  getCourses,
+  getCourseById,
+} = require("../controllers/courseController");
 
 const router = express.Router();
 
-router.post('/', createCourse);
-router.get('/', getCourses);
-router.get('/:id', getCourseById);
+router.post("/", createCourse);
+router.get("/", getCourses);
+router.get("/:id", getCourseById);
 
 module.exports = router;
