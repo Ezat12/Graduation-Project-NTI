d18dc69f8b6fbbdb1ccdcb474fa2b2ca4bea7289
Ezat <ezatelbery164@gmail.com>
2025-10-08 21:17:27 +0300

    setup frontend and create folder instructor

---
+++ client/src/app/instructor/instructor-routing-module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Home } from './components/home/home';
+import { Courses } from './components/courses/courses';
+import { Student } from './components/student/student';
+import { Rating } from './components/rating/rating';
+import { Setting } from './components/setting/setting';
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: Home,
+      },
+      {
+        path: 'courses',
+        component: Courses,
+      },
+      {
+        path: 'student',
+        component: Student,
+      },
+      {
+        path: 'rating',
+        component: Rating,
+      },
+      {
+        path: 'setting',
+        component: Setting,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class InstructorRoutingModule {}
