fc6cdabd425f7eeb86d61d818f866683df60ee0e
Shaimaa <shimaaraslan24@gmail.com>
2025-10-12 14:17:49 +0300

    Done

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { Contact } from './Student/Component/contact/contact';
+import { Courses } from './Student/Component/courses/courses';
 
 export const routes: Routes = [
   {
@@ -6,4 +8,9 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./instructor/instructor-routing-module').then((m) => m.InstructorRoutingModule),
   },
+
+  { path: 'courses', component: Courses , title:"Courses" },
+  { path: 'contact', component: Contact , title:"Contact"},
+      { path: '**', redirectTo: 'Contact',},
+    { path: '', redirectTo: 'courses', pathMatch: 'full' },
 ];
--- server/src/config/connectionDB.js
+++ server/src/config/connectionDB.js
@@ -1,15 +1,29 @@
 const mongoose = require("mongoose");
 
-const connectionDB = () => {
-  mongoose
-    .connect(process.env.DB_URI)
-    .then(() => {
-      console.log("Data base connection");
-    })
-    .catch((e) => {
-      console.log(e);
-      console.log("Error data base");
+// const connectionDB = () => {
+//   mongoose
+//     .connect(process.env.DB_URI)
+//     .then(() => {
+//       console.log("Data base connection");
+//     })
+//     .catch((e) => {
+//       console.log(e);
+//       console.log("Error data base");
+//     });
+// };
+
+const connectionDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/graduationDB", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 
